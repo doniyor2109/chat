fdb79f7a804267d1bfe32161f0817d36c406ac1c
doniyor2109 <doniyor2109@mail.ru>
2016-04-25 19:07:46 +0500

    create new example file

--- example/devtools_page.js
+++ example/devtools_page.js
@@ -1,1 +1,11 @@
+//chating width background
+chat.send({to:SCRIPTS.BACKGROUND,message:{letter:"hello to BACKGROUND from DEVTOOLS"}});
+chat.recieveBackground=function(message){
+    console.log(m.letter);
+};
 
+//chating width content script
+chat.send({to:SCRIPTS.CONTENT_SCRIPT,message:{letter:"hello to CONTENT_SCRIPT from DEVTOOLS"}});
+chat.recieveContent=function(message){
+    console.log(m.letter);
+};
